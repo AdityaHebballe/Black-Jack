7c92727a4f7e63079c681d8dbe100b0bf73845eb
Aditya Hebballe <45328110+AdityaHebballe@users.noreply.github.com>
2023-03-08 22:33:52 +0530

    smtg

--- mymodules/player.py
+++ mymodules/player.py
@@ -1,12 +1,12 @@
 class Chips:
-    def __init__(self):
-        self.total=100
+    def __init__(self,total=100):
         self.betting=0
+        self.total=total
 
     def win_bet(self):
         self.total += self.betting
     
     def lose_bet(self):
         self.total -= self.betting
-    
+
 
