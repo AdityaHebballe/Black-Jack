1b6a9607fc1b25ac6eff2c5e53e8f7ce38fdbe0a
Aditya Hebballe <45328110+AdityaHebballe@users.noreply.github.com>
2023-03-08 21:22:20 +0530

    smtg i did idr

--- mymodules/player.py
+++ mymodules/player.py
@@ -1,13 +1,12 @@
 class Betting:
-    def __init__(self,betting,total=100):
-        self.total=total
-        self.betting=betting
+    def __init__(self):
+        self.total=100
+        self.betting=0
 
     def bet(worl):
         if worl=='l':
             self.total=-self.betting
         elif worl=='w':
             self.total=+self.betting
-    
     
 
